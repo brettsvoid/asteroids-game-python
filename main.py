d7f34ad8c9eeca2fba0c3fef42c11c0ffdb71ffb
Brett Henderson <BrettsVoid@gmail.com>
2024-08-29 00:40:40 +0100

    add player entity

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
 from constants import *
+from player import Player
+
+FPS = 60
 
 
 def main():
@@ -10,6 +13,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,7 +25,13 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        delta = clock.tick(FPS)
+        dt = delta / 1000
 
 
 if __name__ == "__main__":
